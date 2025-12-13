ff837492006328ece4d1f181fa9b5fb557c93cb0
Rahul Kumar <rhlkmr911@outlook.com>
2025-12-13 13:35:49 +0000

    - docked delete and reset button on the bottom for homepage - added dialog form for delete confirmation - docked update, delete, and preview button on bottom for editor - add underline for header links

--- admin/src/Pages/Components/Header.jsx
+++ admin/src/Pages/Components/Header.jsx
@@ -3,13 +3,13 @@ import {Link} from "react-router-dom";
 export default function Header() {
     return (
         <div className="flex border-b-1 text-center gap-2 justify-between p-3 bg-[#5a88af]">
-            <Link to={'/'}>
+            <Link to={'/'} className="hover:underline">
                 Admin Dashboard
             </Link>
 
             <nav className="flex gap-3">
-                <Link to={'/submit'}>Add Game</Link>
-                <Link to={'/batch'}>Add Batch</Link>
+                <Link to={'/submit'} className="hover:underline">Add Game</Link>
+                <Link to={'/batch'} className="hover:underline">Add Batch</Link>
             </nav>
         </div>
     )
