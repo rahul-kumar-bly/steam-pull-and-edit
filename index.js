40591cd15ded1c8642cf2760bb75e64b1e3d876d
Rahul Kumar <rhlkmr911@outlook.com>
2026-01-25 07:40:13 +0000

    middleware and error handler added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import steamRouter from './routes/steam.route.js'
 import addGameRouter from './routes/game.route.js'
+import authRouter from './routes/auth.route.js';
+
 import cors from 'cors'
 
 const app = express()
@@ -20,5 +22,18 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("MongoDB Connected")
 }).catch(err => console.log(err))
 
+
+
 app.use('/steam', steamRouter)
 app.use('/game', addGameRouter)
+app.use('/auth', authRouter) 
+
+app.use ((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
